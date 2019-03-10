b308ce82b742873008014700787014954fe498d3
ming <oneyearice@gmail.com>
2019-03-10 17:39:08 +0800

    1.fail

---
+++ py20190310.py
@@ -0,0 +1,14 @@
+import paramiko
+
+def qytang_ssh(ip,username,password,port=22,cmd='ls'):
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip,port=port,username=username,password=password,timeout=5,compress=True)
+    stdin.stdout.stderr = ssh.exec_command(cmd)
+    x = stdout.read().decode()
+    return x
+
+# if __name__ == '__main__':
+
+print(qytang_ssh('192.168.224.159','root','Cisc0@123'))
