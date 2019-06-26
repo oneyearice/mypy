462db37ba2f3f857ff6556fbc07430087abb1ab9
ming <oneyearice@gmail.com>
2019-06-26 23:02:15 +0800

    6-26,23:02

--- py_phase1/second5_June26/1_查看linux网关.py
+++ py_phase1/second5_June26/1_查看linux网关.py
@@ -9,8 +9,12 @@ route_n_result = os.popen('route -n').read()
 line = route_n_result.split('\n')
 
 gw = re.match('0\.0\.0\.0\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+0\.0\.0\.0\s+UG\s+.*',line[2]).groups()
-
+print(gw)
 print('网关为:'+gw[0])
 
 if __name__ == '__main__':
     pass
+
+
+
+print(test)
--- py_phase1/second5_June26/1_查看linux网关_method2.py
+++ py_phase1/second5_June26/1_查看linux网关_method2.py
@@ -10,6 +10,7 @@ route_n_result = os.popen('route -n').read()
 
 gw = re.findall('0\.0\.0\.0\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+0\.0\.0\.0',route_n_result)
 
+print(gw)
 print('网关为:'+gw[0])
 
 if __name__ == '__main__':
